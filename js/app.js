35b95457d9db3021cd6384caf8b3cee432c666d5
mel1bt <mberrocal.tipte@gmail.com>
2016-09-19 18:22:25 -0500

    actualizando restricciones

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
+
 window.onload = function () {
 	var boton  = document.getElementById("calcular");
     var cadena = "";
+    
+    boton.disabled=true;   
 	boton.addEventListener("click", function() {
         /*
         textArea = document.getElementById("textA").value;
@@ -9,13 +12,29 @@ window.onload = function () {
         crearP.appendChild(nodo);
          var contenedor = document.getElementById("contenedor");
         contenedor.insertBefore(crearP, list.childNodes[0]);
+        
+        document.getElementById("two").classList.add("borde-azul");
         */
         var textArea = document.getElementById("textA").value;
         var crearDiv = document.createElement("p");
         crearDiv.innerHTML = textArea;
         var contenedor = document.getElementById("contenedor");
         contenedor.insertBefore(crearDiv,contenedor.childNodes[0]);
-        document.getElementById("textA").value = "";
+        document.getElementById("textA").value = "";  
+        boton.disabled=true;  
     });
+    
+    var textAr =document.getElementById("textA");
+    textAr.addEventListener("keydown", function(){ 
+        boton.disabled=false; 
+        var numLim = 140;
+        var numLong = document.getElementById("textA").value.length;
+        document.getElementById("contar").innerHTML = numLim - numLong; 
+        if (numLong >= numLim){
+            document.getElementById("contar").classList.add("colorLim");
+            boton.disabled=true;
+        }   
+    });                             
 }
 
+ 
